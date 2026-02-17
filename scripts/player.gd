2b1fffe213225f8d7318cc94be7c2e44d592d055
Diogo <diogoribfaria@gmail.com>
2026-02-17 17:06:01 -0300

    Implement camera rotation and input map

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
 
-const SPEED = 8
+const BASE_SPEED = 5
 const JUMP_VELOCITY = 7
 
+var speed: float
+
+@onready var _camera_pivot: Node3D = $CameraPivot
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,18 +14,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Handle running.
+	if Input.is_action_pressed("run"):
+		speed = BASE_SPEED * 2
+	else:
+		speed = BASE_SPEED
+
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	# Handle camera rotation.
+	if Input.is_action_pressed("ui_left"):
+		_camera_pivot.rotation.y += deg_to_rad(90) * delta
+	elif Input.is_action_pressed("ui_right"):
+		_camera_pivot.rotation.y -= deg_to_rad(90) * delta
