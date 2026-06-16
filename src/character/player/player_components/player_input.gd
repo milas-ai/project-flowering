6d1151bee3a3a6c583fa1823d09e585e538a9ad8
Diogo <diogoribfaria@gmail.com>
2026-06-15 23:33:04 -0300

    fix: edge detection shader and implement camera rotation in player input

--- src/character/player/player_components/player_input.gd
+++ src/character/player/player_components/player_input.gd
@@ -6,6 +6,7 @@ signal jump_pressed
 
 var h_input_dir = Vector2.ZERO
 var running: bool = false
+var camera_rotation: float = deg_to_rad(45)
 
 @onready var _camera_pivot: Node3D = $"../CameraPivot"
 
@@ -15,6 +16,15 @@ func _process(_delta: float) -> void:
 	h_input_dir = h_input_dir.rotated(-1 * _camera_pivot.rotation.y)
 	
 	running = Input.is_action_pressed("run")
+
+	# Handle camera rotation
+	if is_equal_approx(camera_rotation, _camera_pivot.rotation.y):
+			if Input.is_action_pressed("ui_left"):
+					camera_rotation -= deg_to_rad(90)
+			elif Input.is_action_pressed("ui_right"):
+					camera_rotation += deg_to_rad(90)
+	else:
+			_camera_pivot.rotation.y = lerp_angle(_camera_pivot.rotation.y, camera_rotation, 0.1)
 
 
 func _input(event: InputEvent) -> void:
