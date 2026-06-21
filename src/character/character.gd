85248b6b7b65213c803fe44e2564992cfd4bb075
Diogo <diogoribfaria@gmail.com>
2026-06-21 18:49:40 -0300

    feat: enhance character and enemy functionality with health management and attack logic

--- src/character/character.gd
+++ src/character/character.gd
@@ -4,11 +4,17 @@ class_name Character
 
 const BASE_SPEED: float = 5
 const JUMP_VELOCITY: float = 7
+const BASE_HEALTH: int = 100
 
 var vertical_velocity: Vector3 = Vector3.ZERO
 var horizontal_velocity: Vector3 = Vector3.ZERO
 var horizontal_direction: Vector3 = Vector3.ZERO
 var speed: float
+var health: int
+
+
+func _ready() -> void:
+	health = BASE_HEALTH
 
 
 func _physics_process(delta: float) -> void:
@@ -20,3 +26,9 @@ func _physics_process(delta: float) -> void:
 func fall(delta: float) -> void:
 	if not is_on_floor():
 		vertical_velocity += get_gravity() * delta
+
+
+func take_damage(amount: int) -> void:
+	health -= amount
+	if health <= 0:
+		print(self.name + " has died!")
--- src/character/enemies/enemy.gd
+++ src/character/enemies/enemy.gd
@@ -1,5 +1,17 @@
 extends Character
 class_name Enemy
 
+@onready var player: Player = $"../Player"
 
-const friction: float = 10
+var in_cooldown: bool = false
+
+const friction: float = 25
+
+
+func attack_player() -> void:
+	if player.health > 0 and not in_cooldown:
+		print(self.name + " attacks the player!")
+		player.take_damage(10)
+		in_cooldown = true
+		await get_tree().create_timer(2.0).timeout
+		in_cooldown = false
