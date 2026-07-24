18ab1551449cbd4ac84e87d62267a78bf9eecc7b
Diogo <diogoribfaria@gmail.com>
2026-07-24 18:36:30 -0300

    feat: implement State and StateMachine classes

---
+++ util/state.gd
@@ -0,0 +1,26 @@
+extends Node
+
+class_name State
+
+@onready var state_machine: StateMachine = get_parent()
+@onready var body: Node = state_machine.body
+
+var elapsed_time: float = 0.0
+
+func enter() -> void:
+    elapsed_time = 0
+    _enter()
+
+func update(delta: float) -> void:
+    elapsed_time += delta
+    _update(delta)
+
+
+func _enter() -> void:
+    return
+
+func _exit() -> void:
+    return
+
+func _update(_delta: float) -> void:
+    pass
---
+++ util/state_machine.gd
@@ -0,0 +1,32 @@
+extends Node
+
+class_name StateMachine
+
+var state: State = null
+var previous_state: State = null
+
+var states: Dictionary[StringName, State] = {}
+
+@export var body: Node
+
+
+func _ready() -> void:    
+	set_state.call_deferred(get_child(0).name)
+	for child in get_children():
+		states[child.name] = child
+
+
+func _physics_process(delta: float) -> void:
+	if state != null:
+		state.update(delta)
+
+
+func set_state(state_name: StringName) -> void:
+	previous_state = state
+	state = states[state_name]
+	
+	if state != previous_state:
+		if previous_state != null:
+			previous_state._exit()
+		if state != null:
+			state.enter()
