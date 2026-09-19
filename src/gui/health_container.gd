2cfe66e4e777fb2dc374c2b56e1d5d63a2bb905e
Diogo <diogoribfaria@gmail.com>
2026-09-18 21:37:52 -0300

    feat: update health HUD to include empty hearts

--- src/gui/health_container.gd
+++ src/gui/health_container.gd
@@ -1,22 +1,21 @@
 extends HBoxContainer
 
 @onready var hearts: Array[Node] = get_children()
-@onready var full_heart_texture: Texture2D = load("res://assets/gui/heart.png")
+@onready var empty_heart_texture: Texture2D = load("res://assets/gui/empty_heart.png")
 @onready var half_heart_texture: Texture2D = load("res://assets/gui/half_heart.png")
+@onready var full_heart_texture: Texture2D = load("res://assets/gui/full_heart.png")
 
 
 func _on_player_update_health(health, max_health) -> void:
 	for i in range(health/2):
-		hearts[i].visible = true
 		hearts[i].texture = full_heart_texture
 	if health % 2 == 1:
-		hearts[health/2].visible = true
 		hearts[health/2].texture = half_heart_texture
 		for i in range(health/2+1, max_health/2 + max_health%2):
-			hearts[i].visible = false
+				hearts[i].texture = empty_heart_texture
 	else:
 		for i in range(health/2, max_health/2 + max_health%2):
-			hearts[i].visible = false
+			hearts[i].texture = empty_heart_texture
 	
 	#for i in range(max_health/2 + max_health%2):
 		#if (i+1)*2 < health:
