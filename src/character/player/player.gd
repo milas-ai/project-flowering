dac2212a1dce7b123f99dc363aa698ef6f23181c
Diogo <diogoribfaria@gmail.com>
2026-09-19 16:21:43 -0300

    feat: simplify health update logic in health container

--- src/character/player/player.gd
+++ src/character/player/player.gd
@@ -4,13 +4,12 @@ class_name Player
 
 const friction: float = 10
 const slide_friction: float = 1.0
-var max_health = BASE_HEALTH
 
 signal update_health(health)
 
 
 func take_damage(amount: int) -> void:
 	health -= amount
-	emit_signal("update_health", health, max_health)
+	emit_signal("update_health", health)
 	if health <= 0:
 		get_tree().quit()
--- src/gui/health_container.gd
+++ src/gui/health_container.gd
@@ -12,31 +12,17 @@ const MAX_HEALTH = 6
 func _ready() -> void:
 	for i in range(MAX_HEALTH/2 + MAX_HEALTH%2):
 		var texture_rect = TextureRect.new()
-		texture_rect.texture = empty_heart_texture
+		texture_rect.texture = full_heart_texture
 		texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 		texture_rect.expand_mode = TextureRect.EXPAND_FIT_WIDTH
 		add_child(texture_rect)
 		hearts.append(texture_rect)
 
 func _on_player_update_health(health) -> void:
-	for i in range(health/2):
-		hearts[i].texture = full_heart_texture
-	if health % 2 == 1:
-		hearts[health/2].texture = half_heart_texture
-		for i in range(health/2+1, MAX_HEALTH/2 + MAX_HEALTH%2):
-				hearts[i].texture = empty_heart_texture
-	else:
-		for i in range(health/2, MAX_HEALTH/2 + MAX_HEALTH%2):
+	for i in range(MAX_HEALTH/2 + MAX_HEALTH%2):
+		if health >= (i*2+1)+1:
+			hearts[i].texture = full_heart_texture
+		elif health >= i*2+1:
+			hearts[i].texture = half_heart_texture
+		else:
 			hearts[i].texture = empty_heart_texture
-	
-	#for i in range(MAX_HEALTH/2 + MAX_HEALTH%2):
-		#if (i+1)*2 < health:
-			#hearts[i].visible = true
-			#if i+2 < health:
-				#hearts[i].texture = full_heart_texture
-				#i += 1
-			#else:
-				#hearts[i].texture = half_heart_texture
-		#else:
-			#hearts[i].visible = false
-			
