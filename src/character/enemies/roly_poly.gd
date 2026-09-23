da4b548f5b284e75e96c4b9b097ffeddb7aa47db
Diogo <diogoribfaria@gmail.com>
2026-09-22 23:35:29 -0300

    feat: implement base attack logic for roly poly

--- src/character/enemies/roly_poly.gd
+++ src/character/enemies/roly_poly.gd
@@ -3,7 +3,23 @@ class_name RolyPoly
 
 
 const SPEED: float = 5
+const WORLD_LAYER_BITMASK: int = 1 << (2 - 1) # world layer is 2, so we shift 1 left by (2 - 1) = 1 to get the bitmask for layer 2
 
 
 func attack_player() -> void:
-  pass
+  look_at(player.global_transform.origin, Vector3.UP)
+  rotation.x = 0
+  animation_player.stop()
+  animation_player.clear_queue()
+  animation_player.play("attack")
+  get_tree().create_timer(0.72).timeout.connect(func(): horizontal_velocity = -global_transform.basis.z * SPEED)
+  while true:
+    await get_tree().physics_frame
+    for i in get_slide_collision_count():
+      var collider = get_slide_collision(i).get_collider()
+      if collider and "collision_layer" in collider:
+        if collider.collision_layer & ~WORLD_LAYER_BITMASK:
+          # Explosion
+          if player_on_near_sight:
+            player.take_damage(4)
+          queue_free()
