0a0a68e95922f0e07ed43209e230f0ffc5f9cd66
Diogo <diogoribfaria@gmail.com>
2026-09-26 00:46:44 -0300

    refactor: utilize collision layer for pushable objects verification

--- src/character/player/player_components/player_push.gd
+++ src/character/player/player_components/player_push.gd
@@ -4,6 +4,8 @@ class_name PlayerPush
 
 @export var push_force = 10.0
 
+const PUSHABLE_LAYER_BITMASK: int = 1 << (4 - 1)
+
 @onready var player: Player = $"../.."
 @onready var player_input: PlayerInput = $"../../Input"
 @onready var ray_cast = $"../../CharacterModel/RayCast3D"
@@ -22,8 +24,7 @@ func _on_player_input_push_pressed():
 		
 		if ray_cast.is_colliding():
 			var hit_object = ray_cast.get_collider()
-			
-			if hit_object is RigidBody3D:
+			if hit_object.collision_layer & PUSHABLE_LAYER_BITMASK:
 				var push_direction = ray_cast.global_transform.basis.z.normalized()
 				var hit_point = ray_cast.get_collision_point() - hit_object.global_position
 				hit_object.apply_impulse(push_direction * push_force, hit_point)
