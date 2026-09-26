2bfc656aa1c479177598b6ac693a4221e268e13e
Diogo <diogoribfaria@gmail.com>
2026-09-26 00:43:42 -0300

    feat: add verification to prevent multiple push input

--- src/character/player/player_components/player_push.gd
+++ src/character/player/player_components/player_push.gd
@@ -15,17 +15,17 @@ func _ready():
 	ray_cast.enabled = false
 
 func _on_player_input_push_pressed():
-	ray_cast.enabled = true
-	ray_cast.force_raycast_update()
-	is_pushing = true
-	
-	if ray_cast.is_colliding():
-		var hit_object = ray_cast.get_collider()
+	if not is_pushing:
+		is_pushing = true
+		ray_cast.enabled = true
+		ray_cast.force_raycast_update()
 		
-		if hit_object is RigidBody3D:
-			var push_direction = ray_cast.global_transform.basis.z.normalized()
+		if ray_cast.is_colliding():
+			var hit_object = ray_cast.get_collider()
 			
-			var hit_point = ray_cast.get_collision_point() - hit_object.global_position
-			hit_object.apply_impulse(push_direction * push_force, hit_point)
+			if hit_object is RigidBody3D:
+				var push_direction = ray_cast.global_transform.basis.z.normalized()
+				var hit_point = ray_cast.get_collision_point() - hit_object.global_position
+				hit_object.apply_impulse(push_direction * push_force, hit_point)
 
-	get_tree().create_timer(0.46).timeout.connect(func(): is_pushing = false; ray_cast.enabled = false)
+		get_tree().create_timer(0.46).timeout.connect(func(): is_pushing = false; ray_cast.enabled = false)
